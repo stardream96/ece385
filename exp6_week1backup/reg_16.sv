55c0b552bae541700d56fa66d8c6f444f742f6ff
stardream96 <stardream96@gmail.com>
2019-02-22 14:55:27 -0600

    cleaned
    unnecessary file cleaned up.

---
+++ exp6_week1backup/reg_16.sv
@@ -0,0 +1,14 @@
+module reg_16 (input  logic Clk, LD, Reset,
+              input  logic [15:0]  D,
+              output logic [15:0]  Data_Out);
+
+    always_ff @ (posedge Clk)
+    begin
+	 	 if (LD) //notice, this is a sycnrhonous reset, which is recommended on the FPGA
+			  Data_Out <= D;
+		 if(~Reset)
+			  Data_Out <= 16'b0000000000000000;
+
+    end
+
+endmodule
